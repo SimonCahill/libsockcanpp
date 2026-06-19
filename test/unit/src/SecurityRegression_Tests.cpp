06e9fba66cbb2c4dda231764e79b9969618a459b
Simon Cahill <s.cahill@procyon-systems.de>
2026-06-19 08:38:37 +0000

    Test now explicitly tests if lib crashes on too large ifnamesize

--- test/unit/src/SecurityRegression_Tests.cpp
+++ test/unit/src/SecurityRegression_Tests.cpp
@@ -12,8 +12,6 @@
 
 #include <linux/can.h>
 #include <net/if.h>
-#include <sys/socket.h>
-#include <unistd.h>
 
 #include <string>
 #include <system_error>
@@ -22,14 +20,6 @@ using sockcanpp::CanDriver;
 using sockcanpp::CanFdMessage;
 using sockcanpp::CanMessage;
 using sockcanpp::exceptions::CanInitException;
-
-#if defined(__SANITIZE_ADDRESS__)
-#define LIBSOCKCANPP_HAS_ASAN 1
-#elif defined(__has_feature)
-#if __has_feature(address_sanitizer)
-#define LIBSOCKCANPP_HAS_ASAN 1
-#endif
-#endif
 
 TEST(SecurityRegressionTests, RawClassicCanMessageRejectsOversizedDlc) {
     can_frame frame{};
@@ -67,18 +57,5 @@ TEST(SecurityRegressionTests, RawCanFdMessageRejectsOversizedLength) {
 TEST(SecurityRegressionTests, OversizedInterfaceNameIsRejectedBeforeIfreqCopy) {
     const std::string oversizedInterfaceName(IFNAMSIZ, 'a');
 
-#if defined(LIBSOCKCANPP_HAS_ASAN)
-    const int probeSocket = socket(PF_CAN, SOCK_RAW, CanDriver::CAN_SOCK_RAW);
-    if (probeSocket < 0) {
-        GTEST_SKIP() << "Raw CAN sockets are not available in this environment";
-    }
-    close(probeSocket);
-
-    EXPECT_DEATH({
-        const CanDriver driver(oversizedInterfaceName, CanDriver::CAN_SOCK_RAW);
-        static_cast<void>(driver);
-    }, "");
-#else
     EXPECT_THROW(CanDriver(oversizedInterfaceName, CanDriver::CAN_SOCK_RAW), CanInitException);
-#endif
 }
